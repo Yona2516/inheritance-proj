d129ee38c59f5cb5f511c181d1daa23862741cb6
Yona2516 <yonamgongolwa@gmail.com>
2025-07-03 01:29:10 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',          // use your MySQL user
-  password: '',          // use your MySQL password
+  password: '@Eliakimu122',          // use your MySQL password
   database: 'inheritance'
 });
 
